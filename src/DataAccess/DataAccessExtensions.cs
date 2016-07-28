48e8db4f6e5ba944e60e849ba86a96081283a2d9
Victor.X.Qu <victor.x.qu@newegg.com>
2016-07-28 13:14:23 +0800

    init

---
+++ src/DataAccess/DataAccessExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Common;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace VIC.DataAccess
+{
+    public static class DataAccessExtensions
+    {
+        public static IServiceCollection UseDataAccess(IServiceCollection service)
+        {    
+
+            return service;
+        }
+    }
+}
