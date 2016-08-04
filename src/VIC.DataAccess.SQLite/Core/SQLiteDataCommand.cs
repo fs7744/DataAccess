cb2ecdb15eb74c394eac73494c9b56ae1c4d5dfe
Victor.X.Qu <victor.x.qu@newegg.com>
2016-08-04 09:53:46 +0800

    update

---
+++ src/VIC.DataAccess.SQLite/Core/SQLiteDataCommand.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data.Common;
+using VIC.DataAccess.Abstraction.Converter;
+using VIC.DataAccess.Core;
+
+namespace VIC.DataAccess.SQLite.Core
+{
+    public class SQLiteDataCommand : DataCommand
+    {
+        public SQLiteDataCommand(IParamConverter pc, IScalarConverter sc, IEntityConverter ec) : base(pc, sc, ec)
+        {
+        }
+
+        protected override DbConnection CreateConnection(string connectionString)
+        {
+            return new SqliteConnection(connectionString);
+        }
+    }
+}
---
+++ src/VIC.DataAccess.SQLite/DataAccessExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.Extensions.DependencyInjection;
+using VIC.DataAccess.Abstraction;
+using VIC.DataAccess.Abstraction.Converter;
+using VIC.DataAccess.Core;
+using VIC.DataAccess.Core.Converter;
+using VIC.DataAccess.SQLite.Core;
+
+namespace VIC.DataAccess
+{
+    public static class DataAccessExtensions
+    {
+        public static IServiceCollection UseDataAccess(this IServiceCollection service)
+        {
+            TypeHelper.SqlParameterType = typeof(SqliteParameter);
+            return service.AddSingleton<IDbFuncNameConverter, DbFuncNameConverter>()
+                .AddSingleton<IDbTypeConverter, DbTypeConverter>()
+                .AddSingleton<IScalarConverter, ScalarConverter>()
+                .AddSingleton<IEntityConverter, EntityConverter>()
+                .AddSingleton<IParamConverter, ParamConverter>()
+                .AddTransient<IDataCommand, SQLiteDataCommand>();
+        }
+    }
+}
