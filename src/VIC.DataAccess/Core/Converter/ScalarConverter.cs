3c92a6ffde246d1b3dd516336d2616c813e30ae7
Victor.X.Qu <fs7744@hotmail.com>
2016-08-07 00:47:49 +0800

    ut datapatameter

--- src/VIC.DataAccess/Core/Converter/ScalarConverter.cs
+++ src/VIC.DataAccess/Core/Converter/ScalarConverter.cs
@@ -21,22 +21,23 @@ namespace VIC.DataAccess.Core.Converter
                 { typeof(short), i => i.GetInt16(0)},
                 { typeof(byte), i => i.GetByte(0)},
                 { typeof(Guid), i => i.GetGuid(0)},
-                { typeof(long?), i => i.IsDBNull(0) ? (long?)i.GetInt64(0) : null},
-                { typeof(bool?), i => i.IsDBNull(0) ? (bool?)i.GetBoolean(0) : null},
-                { typeof(DateTime?), i => i.IsDBNull(0) ? (DateTime?)i.GetDateTime(0) : null},
-                { typeof(decimal?), i => i.IsDBNull(0) ? (decimal?)i.GetDecimal(0) : null},
-                { typeof(double?), i => i.IsDBNull(0) ? (double?)i.GetDouble(0) : null},
-                { typeof(int?), i => i.IsDBNull(0) ? (int?)i.GetInt32(0) : null},
-                { typeof(float?), i => i.IsDBNull(0) ? (float?)i.GetFloat(0) : null},
-                { typeof(short?), i => i.IsDBNull(0) ? (short?)i.GetInt16(0) : null},
-                { typeof(byte?), i => i.IsDBNull(0) ? (byte?)i.GetByte(0) : null},
-                { typeof(Guid?), i => i.IsDBNull(0) ? (Guid?)i.GetGuid(0) : null},
+                { typeof(long?), i => i.IsDBNull(0) ? null : (long?)i.GetInt64(0)},
+                { typeof(bool?), i => i.IsDBNull(0) ?  null : (bool?)i.GetBoolean(0)},
+                { typeof(DateTime?), i => i.IsDBNull(0) ?  null : (DateTime?)i.GetDateTime(0) },
+                { typeof(decimal?), i => i.IsDBNull(0) ?  null : (decimal?)i.GetDecimal(0) },
+                { typeof(double?), i => i.IsDBNull(0) ?  null : (double?)i.GetDouble(0) },
+                { typeof(int?), i => i.IsDBNull(0) ?  null : (int?)i.GetInt32(0) },
+                { typeof(float?), i => i.IsDBNull(0) ?  null : (float?)i.GetFloat(0) },
+                { typeof(short?), i => i.IsDBNull(0) ?  null : (short?)i.GetInt16(0) },
+                { typeof(byte?), i => i.IsDBNull(0) ?  null : (byte?)i.GetByte(0) },
+                { typeof(Guid?), i => i.IsDBNull(0) ?  null : (Guid?)i.GetGuid(0) },
             };
 
         public dynamic Convert<T>(DbDataReader reader)
         {
             Func<DbDataReader, dynamic> func = null;
-            return _SCs.TryGetValue(typeof(T), out func) ? func(reader) : default(T);
+            T result = _SCs.TryGetValue(typeof(T), out func) ? (T)func(reader) : default(T);
+            return result;
         }
     }
 }
