40938f209c8e8bbb9fc91b41da90c16ba7b0252b
Victor.X.Qu <fs7744@hotmail.com>
2016-08-08 17:03:44 +0800

    add mssql example

---
+++ example/MSSqlExample/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MSSqlExample
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? JoinDate { get; set; }
+
+        public decimal? Money { get; set; }
+    }
+}
