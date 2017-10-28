75e34b7ede0d00a16d371ce163c5dd9128e52fed
Victor.X.Qu (g-mis.cncd02.Newegg) 42377 <victor.x.qu@newegg.com>
2017-10-28 16:50:25 +0800

    add IConnectionStringProvider

--- src/VIC.DataAccess.Config/ConfigExtensions.cs
+++ src/VIC.DataAccess.Config/ConfigExtensions.cs
@@ -9,17 +9,17 @@ namespace VIC.DataAccess.Config
 {
     public static class ConfigExtensions
     {
-        public static IServiceCollection UseDataAccessConfig(this IServiceCollection service, string basePath, bool isWatch = false, DbConfig[] others = null, params string[] xmlFiles)
+        public static IServiceCollection UseDataAccessConfig(this IServiceCollection service, string basePath, bool isWatch = false, DbConfig[] others = null, IConnectionStringProvider provider = null, params string[] xmlFiles)
         {
             var config = new PhysicalFileConfigBuilder()
                 .SetBasePath(basePath)
-                .Add(new XmlConfigFileProvider<DbConfig>(DbManager.DbConfigKey, isWatch, i => BuildConfig(i, others), xmlFiles))
+                .Add(new XmlConfigFileProvider<DbConfig>(DbManager.DbConfigKey, isWatch, i => BuildConfig(i, others, provider), xmlFiles))
                 .Build();
             service.AddSingleton<IDbManager>(i => new DbManager(config, i));
             return service;
         }
 
-        public static async Task<DbConfig> BuildConfig(Task<DbConfig>[] vs, DbConfig[] others)
+        public static async Task<DbConfig> BuildConfig(Task<DbConfig>[] vs, DbConfig[] others, IConnectionStringProvider provider)
         {
             var config = new DbConfig()
             {
@@ -39,6 +39,10 @@ namespace VIC.DataAccess.Config
                 var c = await i;
                 c?.ConnectionStrings?.ForEach(x => css.Add(x.Name, x.ConnectionString));
                 c?.SqlConfigs?.ForEach(x => config.Sqls.Add(x.CommandName, x));
+            }
+            if (provider != null)
+            {
+                provider.Update(css);
             }
             foreach (var item in config.Sqls.Values)
             {
